4cb2f1fd9a0c5ca9061686509f120888ea67fc9c
Anuchit Yai-in <anuchit.im@gmail.com>
2019-02-25 16:23:38 +0700

    [red] Unittest call api /carlist/count/

---
+++ src/Api.test.js
@@ -0,0 +1,22 @@
+import Api from './Api'
+global.fetch = require('jest-fetch-mock')
+
+describe('testing api', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
+  })
+ 
+  it('calls api get counter', () => {
+
+    fetch.mockResponseOnce(JSON.stringify({ count: 1 }))
+ 
+    //assert on the response
+    Api.get('/carlist/count/').then(res => {
+
+      expect(res.count).toEqual(1)
+    })
+ 
+    //assert on the times called and arguments given to fetch
+    expect(fetch.mock.calls.length).toEqual(1)
+  })
+})
