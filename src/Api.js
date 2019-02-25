c3b605dba8094a60be0ff04c9e0bcbc00ca91462
Anuchit Yai-in <anuchit.im@gmail.com>
2019-02-25 17:43:20 +0700

    [green] api get

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,23 @@
 //api.js
-const baseUrl = 'http:127.0.0.1:8000'
+const baseUrl = 'http://127.0.0.1:8000'
 export default {
-    get : function (path) {
-        return fetch(baseUrl + path).then(res => res.json())
+    get : function (path,cb) {
+      var headers = new Headers();
+      var status = 200
+      fetch(baseUrl+path, {
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'omit',
+        redirect: 'manual',
+        headers: headers
+      }).then((response) => {
+        status = response.status
+        return response.json()
+        
+      }).then((resJson) => {
+        cb(status==200, resJson,status);
+      }).catch((e) => {
+        cb(false, e,status);
+      });
       }
 }
