f081ecbc9b63fd32a808db55d38987d3b0d3bb81
Anuchit Yai-in <anuchit.im@gmail.com>
2019-02-27 09:56:02 +0700

    [red] check render RFIDPopup

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from './App'
 import { shallow, mount } from 'enzyme';
 import CheckInPoup from './components/CheckinPopup'
+import RFIDPopup from './components/RFIDPopup'
 global.fetch = require('jest-fetch-mock')
 
 describe("render", () => {
@@ -10,6 +11,7 @@ describe("render", () => {
     expect(wrapper.find('#btnCheckin')).toHaveLength(1)
     expect(wrapper.find('#btnCheckout')).toHaveLength(1)
     expect(wrapper.find(CheckInPoup)).toHaveLength(1)
+    expect(wrapper.find(RFIDPopup)).toHaveLength(1)
     expect(wrapper.find('#car_amount')).toHaveLength(1)
     expect(wrapper.find('#car_amount').text()).toEqual('0')
   });
